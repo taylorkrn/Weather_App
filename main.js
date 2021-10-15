6c9cf96c69e6be8d4e3e5d47e3d6df0cefb2e3b0
Kieran Taylor <taylor.krn@gmail.com>
2021-10-15 19:02:48 +0200

    CSS update with media screen adjustments

--- main.js
+++ main.js
@@ -5,26 +5,21 @@ $(document).ready(function() {
             const weatherData = data.current;
 
             var iconString = '.' + weatherData.condition.icon.substring(20);
+            idString = '<img src=' + iconString + '>';
+            idString += '<p>' + weatherData.condition.text + '</div>';
+            $("#IconDescription").html(idString);
 
-            var updated = weatherData.last_updated.split(' ');
+            var tempString = '<p><strong>Temp:</strong> ' + weatherData.temp_c + ' C</p>';
+            tempString += '<p><strong>Feel:</strong> ' + weatherData.feelslike_c + ' C</p>';
+            $("#Temperature").html(tempString);
 
-            var string = '<div style="grid-area: 1/1/2/2">Updated: ' + updated[1] + '</div>';
+            var wpString = '<p><strong>Wind:</strong> ' + weatherData.wind_dir + ' ' + weatherData.wind_kph + 'Kph </p>';
+            wpString += '<p><strong>Prec:</strong> ' + weatherData.precip_mm + 'mm</p>';
+            $("#WindPrecip").html(wpString);
 
-            string += '<img src=' + iconString + ' style="grid-area: 2/1/4/2">';
-
-            string += '<div style="grid-area: 4/1/5/2">' + weatherData.condition.text + '</div>';
-
-            string += '<div style="grid-area: 1/2/2/3">Temperature: ' + weatherData.temp_c + ' C</div>';
-
-            string += '<div style="grid-area: 2/2/3/3">Feels like: ' + weatherData.feelslike_c + ' C</div>';
-
-            string += '<div style="grid-area: 3/2/4/3">Wind: ' + weatherData.wind_dir + ' ' + weatherData.wind_kph + 'Kph </div>';
-
-            string += '<div style="grid-area: 4/2/5/3">Precipitation: ' + weatherData.precip_mm + 'mm</div>';
-
-            $("#show-weather").html(string);
             if (status === "success"){
-                $("#get-weather-button").html("Update Current Weather");
+                var updated = weatherData.last_updated.split(' ');
+                $("#get-weather-button").html("Update <span style='color: grey'>(" + updated[1] + ")</span>");
             }
         });
     });
@@ -33,24 +28,20 @@ $(document).ready(function() {
 
     $("#get-forecast-button").on('click', function() {
         $.get('http://api.weatherapi.com/v1/forecast.json?key=9b35f3bc34574268a6b120401211410&q=Berlin&days=5&aqi=no&alerts=no', function(data, status) {
-            var string = '';
 
             const forecastArray = data.forecast.forecastday;
 
             for (let i = 0; i < forecastArray.length; i++){
-                var string = "";
-
-                var date = forecastArray[i].date.split("-");
-
-                string += "<div style='grid-item: 1'>"+ date[2] + "/" + date[1] +"</div>";
+                const date = forecastArray[i].date.split("-");
+                var string = '<p id="Date">' + date[2] + "/" + date[1] + '</p>';
 
                 const weatherData = forecastArray[i].day;
+                const iconString = '.' + weatherData.condition.icon.substring(20);
+                string += '<img class="Icon" src=' + iconString + '>';
 
-                var iconString = '.' + weatherData.condition.icon.substring(20);
+                $(".day-box").eq(i).html(string);
 
-                string += '<img style="grid-item: 2" src=' + iconString + '>';
-
-                string += '<div style="grid-iteam: 3">' + weatherData.condition.text + '</div>';
+                // string += '<p class="day-item"><small>' + weatherData.condition.text + '</small></p>';
 
                 // string += '<div style="grid-area: 1/2/2/3">Max:' + weatherData.maxtemp_c+ ' C, Min: ' + weatherData.mintemp_c + ' C</div>';
 
@@ -59,10 +50,7 @@ $(document).ready(function() {
                 // string += '<div style="grid-area: 3/2/4/3">Max Wind: ' + weatherData.maxwind_kph + 'Kph </div>';
 
                 // string += '<div style="grid-area: 4/2/5/3">Chance:' + weatherData.daily_chance_of_rain + '%, Precipitation: ' + weatherData.totalprecip_mm + 'mm</div>';
-
-                $(".forecast-box").eq(i).html(string);
             };
-            $("#show-forecast").html(string);
             if (status === "success"){
                 $("#get-forecast-button").html("Update Forecast");
             }
